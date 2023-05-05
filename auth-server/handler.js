50b003ab6cd2c42c8502b5d62d7845e8638f7cb1
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-05-05 08:53:30 -0400

    trying to remove 400 error

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,10 +1,8 @@
-const {google} = require("googleapis");
+const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
-const calander = google.calendar("v3");
-
+const calendar = google.calendar("v3");
 /**
- * SCOPES allows you to set access levels; 
- * this is set to read only for now because you don't have access rights to
+ * SCOPES allows you to set access levels; this is set to read only for now because you don't have access rights to
  * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
  */
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
@@ -17,16 +15,14 @@ const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  calender_id: process.env.CALENDAR_ID,
+  calendar_id: process.env.CALENDAR_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://SethSingletary.github.io/meet/"],
   javascript_origins: ["https://SethSingletary.github.io", "http://localhost:3000"],
 };
-
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
-
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
@@ -40,8 +36,7 @@ const oAuth2Client = new google.auth.OAuth2(
  * as a URL parameter.
  *
  */
-
-module.exports.getAuthURL = async() => {
+module.exports.getAuthURL = async () => {
   /**
    *
    * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
@@ -49,20 +44,19 @@ module.exports.getAuthURL = async() => {
    *  scopes are the ones users will see when the consent screen is displayed to them.
    *
    */
-
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
   });
 
-  return{
+  return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allowed-Origin": "*",
-      "Access-Control-Allowed-Credentials": true,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify({
-      authUrl: authUrl
+      authUrl: authUrl,
     }),
   };
 };
