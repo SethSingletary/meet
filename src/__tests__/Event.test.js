fadda9436fc6b5ff8da453710965c5187496dab0
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-05-15 13:15:33 -0400

    update

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -4,20 +4,24 @@ import { mockData } from "../mock-data";
 import { shallow } from "enzyme";
 
 describe('<Event/> Compomnent', () => {
+
     const event = mockData[0];
+    let EventWrapper;
+
+    beforeAll(() => {
+      EventWrapper = shallow(<Event event={event}/>);
+    })
+
     test('renders all needed data while collapsed', () => {
-        const EventWrapper = shallow(<Event event={event}/>);
         expect(EventWrapper.find('.title').text()).toBe(event.summary);
         expect(EventWrapper.find('.location').text()).toBe(event.location);
         expect(EventWrapper.find('.time').text()).toBe(new Date(event.start.dateTime).toString());
         expect(EventWrapper.find('.detailsButton')).toHaveLength(1);
     });
     test('renders details button', () => {
-        const EventWrapper = shallow(<Event event={event}/>);
         expect(EventWrapper.find('.detailsButton')).toHaveLength(1);
     });
     test("expand details, when clicking show details", () => {
-        const EventWrapper = shallow(<Event event={event}/>);
         const detailsButton = EventWrapper.find(".detailsButton");
         expect(detailsButton.text()).toBe("show details");
         expect(EventWrapper.find(".link")).toHaveLength(0);
@@ -26,7 +30,6 @@ describe('<Event/> Compomnent', () => {
         expect(EventWrapper.state("collapsed")).toBe(false);
       });
       test("collapse details, when clicking hide details", () => {
-        const EventWrapper = shallow(<Event event={event}/>);
         EventWrapper.setState({ collapsed: false });
         const detailsButton = EventWrapper.find(".detailsButton");
         expect(detailsButton.text()).toBe("hide details");
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,19 +3,21 @@ import NumberOfEvents from "../NumberOfEvents";
 import { shallow } from "enzyme";
 
 describe('<NumberOfEvents /> Compoment', () => {
+    
+    let NumberOfEventsWrapper, query;
+
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+        query = NumberOfEventsWrapper.state('query');
+    })
+
     test('Render default input', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
         expect(NumberOfEventsWrapper.find('.count')).toHaveLength(1);
     });
     test('Check default number', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
-        const query = NumberOfEventsWrapper.state('query');
         expect(NumberOfEventsWrapper.find('.count').prop('value')).toBe(query);
     })
     test('Check acceptance of diffrent amount', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
-        const query = NumberOfEventsWrapper.state('query');
-
         NumberOfEventsWrapper.setState({
             query: 32
           });
