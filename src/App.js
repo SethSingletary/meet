13b9501e66099daa78978649cb3e2d1ba791bd09
James Katarikawe <jpkatarikawe@gmail.com>
2023-05-29 13:56:01 -0500

    place filters at the top

--- src/App.js
+++ src/App.js
@@ -32,12 +32,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <EventList events={this.state.events} />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents />
+        <EventList events={this.state.events} />
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+import axios from "axios";
 import { mockData } from "./mock-data";
 import NProgress from "nprogress";
 
