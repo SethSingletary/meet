838e2cb40abc05a5d82c00290ad2b73e526da013
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-06-12 16:01:50 -0400

    one test fails, however it has not been touched

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -5,6 +5,8 @@ import React from 'react';
 import { mockData } from '../mock-data';
 import CitySearch from '../CitySearch';
 import { extractLocations } from '../api';
+import NumberOfEvents from '../NumberOfEvents';
+import Event from '../Event';
 
 
 
@@ -22,12 +24,13 @@ defineFeature(feature, test => {
   
 	  then('the user should see the list of upcoming events.', () => {
 		AppWrapper.update();
-		expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
+		expect(AppWrapper.find(Event)).toHaveLength(mockData.length);
 	  });
 	});
   
 	test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
 	  let CitySearchWrapper;
+	  let locations = extractLocations(mockData);
 	  given('the main page is open', () => {
 		CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations}/>)
 	  });
@@ -37,29 +40,35 @@ defineFeature(feature, test => {
 	  });
   
 	  then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-		expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
+		expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(1);
 	  });
 	}); 
   
 	test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-	  given('user was typing “Berlin” in the city textbox', () => {
+	  let AppWrapper;
+	  given('user was typing “Berlin” in the city textbox', async () => {
+		AppWrapper = await mount(<App/>);
+		AppWrapper.find('.city').simulate('change', { target: {value: 'Tokyo'}})
   
 	  });
   
 	  and('the list of suggested cities is showing', () => {
+		AppWrapper.update();
+		expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
   
 	  });
   
 	  when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-  
+		AppWrapper.find('.suggestions li').at(0).simulate('click');
 	  });
   
 	  then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-  
+		const CitySearchWrapper = AppWrapper.find(CitySearch);
+		expect(CitySearchWrapper.state('query')).toBe('Tokyo, Japan');
 	  });
   
 	  and('the user should receive a list of upcoming events in that city', () => {
-  
+		expect(AppWrapper.find(Event)).toHaveLength(mockData.length);
 	  });
 	});
   });
