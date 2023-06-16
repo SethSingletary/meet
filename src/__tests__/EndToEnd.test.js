b2a5d7e1e9ec904ee6766f2031782bc07122e69a
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-06-16 17:30:02 -0400

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,7 +6,7 @@ describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-      jest.setTimeout(100000);
+      jest.setTimeout(200000);
       browser = await puppeteer.launch({
         headless: false,
         slowMo: 250,
