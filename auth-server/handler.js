4ac0c0c3d154d36c72c8a96d351b32449e02f23e
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-06-20 21:10:51 -0400

    changed redirect url

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://meet-i8ljxpaef-sethsingletary.vercel.app/"],
+  //redirect_uris: ["https://meet-i8ljxpaef-sethsingletary.vercel.app/"],
+  redirect_uris: ["http://meet-eta-six.vercel.app/"],
   javascript_origins: ["https://SethSingletary.github.io", "http://localhost:3000", "https://meet-beta-one.vercel.app/", "https://meet-eta-six.vercel.app"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
