bc4f0b4236eaf806344f69122ede866d57ce1b7b
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-07-06 08:53:32 -0400

    Update EventGenresChart.js

--- src/EventGenresChart.js
+++ src/EventGenresChart.js
@@ -7,22 +7,33 @@ import {
 
 const EventGenresChart = ({ events }) => {
     const [data, setData] = useState([]);
-    const genres = ['React', 'Javascript', 'Node', 'jQuery', 'Angular'];
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
 
     useEffect(() => {
         setData(getData());
       }, [`${events}`]);
 
     const getData = () => {
-        genres.map(genre => {
+        let tempData = genres.map(genre => {
             const filteredEvents = events.filter(event => event.summary.includes(genre));
+            //console.log(filteredEvents);
             return {
                 name: genre,
                 value: filteredEvents.length
             }
         })
-        return genres
+        //console.log(tempData);
+        return tempData
     }
+
+    const data01 = [
+      { name: 'Group A', value: 400 },
+      { name: 'Group B', value: 300 },
+      { name: 'Group C', value: 300 },
+      { name: 'Group D', value: 200 },
+      { name: 'Group E', value: 278 },
+      { name: 'Group F', value: 189 },
+    ];
 
     return (
         <ResponsiveContainer width="99%" height={400}>
